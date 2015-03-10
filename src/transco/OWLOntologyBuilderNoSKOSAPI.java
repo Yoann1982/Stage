45e434e0fddd7d30abe213f4502de1e11dd12c9d
Yoann Keravec <yoann.keravec@gmail.com>
2015-03-10 15:08:42 +0100

    Suite

--- src/transco/OWLOntologyBuilderNoSKOSAPI.java
+++ src/transco/OWLOntologyBuilderNoSKOSAPI.java
@@ -4,7 +4,6 @@
 package transco;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Set;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
@@ -13,25 +12,15 @@ import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAnnotation;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
-import org.semanticweb.owlapi.model.OWLClassExpression;
 import org.semanticweb.owlapi.model.OWLDataFactory;
-import org.semanticweb.owlapi.model.OWLEntity;
 import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
-import org.semanticweb.owlapi.model.OWLObjectProperty;
 import org.semanticweb.owlapi.model.OWLObjectPropertyAssertionAxiom;
-import org.semanticweb.owlapi.model.OWLObjectPropertyDomainAxiom;
-import org.semanticweb.owlapi.model.OWLObjectPropertyRangeAxiom;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
 import org.semanticweb.owlapi.model.RemoveAxiom;
-import org.semanticweb.skos.SKOSAnnotation;
-import org.semanticweb.skos.SKOSConcept;
-import org.semanticweb.skos.SKOSEntity;
-import org.semanticweb.skos.SKOSLiteral;
-import org.semanticweb.skos.SKOSUntypedLiteral;
 
 /**
  * @author y.keravec
@@ -42,7 +31,11 @@ public class OWLOntologyBuilderNoSKOSAPI {
 	private OWLDataFactory fact = null;
 	private OWLOntologyManager manager;
 	private OWLOntology targetOntology;
-
+	private OWLClass classePrincipale = null;
+	private OWLClass classeSecondaire = null;
+	private IRI iriProp = null;
+	private IRI iriIndiv = null; 
+			
 	public OWLOntologyBuilderNoSKOSAPI() {
 
 		try {
@@ -167,22 +160,18 @@ public class OWLOntologyBuilderNoSKOSAPI {
 	 * @return IRI (partie Scheme et SchemeSpecificPart) des classes
 	 */
 	public IRI foundIriProject() {
-		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-		// OWLDataFactory fact = manager.getOWLDataFactory();
 
 		IRI iriClasseFille = null;
 
 		Set<OWLNamedIndividual> listIndivOnto = originalOntology
 				.getIndividualsInSignature();
 
-		// Set<OWLClassExpression> listeClasseExpression = fact.get
 		// Si la liste de sous classe est vide, on ne traite pas (c'est que
 		// Concept n'est père d'aucun autre concept
 
 		if (listIndivOnto.size() != 0) {
 
-			// On va pour chaque relation SubClassOf retrouvée, créer un
-			// équivalent SKOS:Broader
+			// On récupère l'IRI du premier individu lu
 			for (OWLNamedIndividual cls : listIndivOnto) {
 				iriClasseFille = cls.getIRI();
 				break;
@@ -244,6 +233,126 @@ public class OWLOntologyBuilderNoSKOSAPI {
 		this.manager.applyChange(new AddAxiom(targetOntology, ax));
 	}
 
+	/**
+	 * Cette méthode ajoute dans l'ontologie cible un axiom de type ObjectProperty
+	 * @param iriObjProp IRI de la relation
+	 * @param domain Classe correspondante au domain de la relation
+	 * @param range Classe correspondante au range de la relation
+	 */
+	public void addObjectProperty(IRI iriObjProp, OWLClass domain, OWLClass range) {
+		// 1 - if broader
+		if (iriObjProp != null
+				&& iriObjProp.toURI().getFragment().equals("broader")) {
+			/**
+			 * 1 - On regarde si la classe est SubClassOf Thing Si
+			 * c'est le cas, on supprime cet axiome
+			 * 
+			 * 2 - On la rattache à Thing si elle n'est pas déjà
+			 * présente dans l'ontologie 3 - On crée la relation
+			 * SubClassOf entre la classe que l'on traite et cette
+			 * deuxième classe.
+			 */
+
+			// 1 - On regarde si la classe est SubClassOf Thing
+
+			// On la recherche dans le factory
+			OWLSubClassOfAxiom rechercheThing = fact
+					.getOWLSubClassOfAxiom(domain,
+							fact.getOWLThing());
+			if (rechercheThing.getSuperClass() != null) {
+
+				RemoveAxiom removeAxiom = new RemoveAxiom(
+						targetOntology, rechercheThing);
+				manager.applyChange(removeAxiom);
+			}
+
+			// 2 - On ajoute classeCree2 à Thing si elle n'existe
+			// pas
+			// dans l'ontologie
+			addToThing(range);
+
+			// 3 - On crée l'axiome SubClassOf entre owlClass et
+			// classe2
+			addSubClassOf(domain, range);
+		}
+	}
+	
+	/**
+	 * Cette méthode à partir d'un individu, recherche les relations dans laquelle cet individu intervient
+	 * et crée l'ensemble de ces relations (ainsi que les classes associées).
+	 * @param individu L'individu que l'on traite.
+	 */
+	public void addRelation(OWLNamedIndividual individu ) {
+		Set<OWLAxiom> listeAxiom = individu
+				.getReferencingAxioms(originalOntology);
+		for (OWLAxiom curAxiom : listeAxiom) {
+			// Ne s'applique qu'au type ObjectPropertyAssertion pour
+			// récupérer les broader
+			if (curAxiom.getAxiomType().toString()
+					.equals("ObjectPropertyAssertion")) {
+
+				// 1 - On récupère le domaine (Subject), on en fait une
+				// classe
+				// 2 - On récupère le range (Object), on en fait une classe
+				// 3 - On récupère la propriété (Property), on en récupère
+				// l'IRI
+
+				OWLObjectPropertyAssertionAxiom curAxiom2 = (OWLObjectPropertyAssertionAxiom) curAxiom;
+
+				classePrincipale = fact.getOWLClass(curAxiom2.getSubject()
+						.asOWLNamedIndividual().getIRI());
+
+				classeSecondaire = fact.getOWLClass(curAxiom2.getObject()
+						.asOWLNamedIndividual().getIRI());
+
+				iriProp = curAxiom2.getProperty().getNamedProperty()
+						.getIRI();
+				
+				addObjectProperty(iriProp, classePrincipale, classeSecondaire);
+			}
+		}
+	}
+	
+	public void addAnnotation(OWLNamedIndividual individu) {
+		Set<OWLAnnotation> listAnnotIndiv = individu.getAnnotations(originalOntology);
+		for (OWLAnnotation curAnnotIndiv : listAnnotIndiv) {
+			OWLLiteral litteral = (OWLLiteral) curAnnotIndiv.getValue();
+
+			// On transcode en fonction du type d'annotation
+
+			String typeAnnot = curAnnotIndiv.getProperty().getIRI().toURI()
+					.getFragment();
+
+			String value = litteral.getLiteral();
+
+			// On crée la classe à laquelle on veut rattacher l'annotation
+			OWLClass classeAnot = fact.getOWLClass(iriIndiv);
+			
+			switch (typeAnnot) {
+			case "prefLabel":
+				// System.out.println("Transcodage de prefLabel -> label");
+				addLabel(classeAnot, value);
+				break;
+			case "altLabel":
+				// System.out.println("Transcodage de altLabel -> label");
+				addLabel(classeAnot, value);
+				break;
+			case "definition":
+				// System.out.println("Transcodage de definition -> comment");
+				addComment(classePrincipale, value);
+				break;
+			case "scopeNote":
+				// System.out.println("Transcodage de scopeNote -> comment");
+				addComment(classePrincipale, value);
+				break;
+			default:
+				System.out.println("Le type " + typeAnnot
+						+ " n'est pas supporté.");
+				break;
+			}
+		}
+	}
+	
 	public void createOntology() {
 
 		// On récupère l'iri de l'ontologie cible
@@ -256,145 +365,14 @@ public class OWLOntologyBuilderNoSKOSAPI {
 				.getIndividualsInSignature();
 		for (OWLNamedIndividual indivCursor : listClassOnto) {
 
-			OWLClass classePrincipale = null;
-			OWLClass classeSecondaire = null;
-			IRI iriProp = null;
+			iriIndiv = indivCursor.getIRI();
 
 			// On traite dans un premier temps les axioms pour créer les
 			// SubClassOf
-
-			IRI iriIndiv = indivCursor.getIRI();
-
-			Set<OWLAxiom> listeAxiom = indivCursor
-					.getReferencingAxioms(originalOntology);
-			boolean debug = false;
-			if (iriIndiv.toURI().getFragment().equals("5")) {
-				System.out.println("INDIVIDU : " + indivCursor.toString());
-				debug = true;
-			}
-			for (OWLAxiom curAxiom : listeAxiom) {
-				if (debug)
-					System.out.println("Axiom : " + curAxiom.toString());
-
-				// Ne s'applique qu'au type ObjectPropertyAssertion pour
-				// récupérer les broader
-				if (curAxiom.getAxiomType().toString()
-						.equals("ObjectPropertyAssertion")) {
-
-					// 1 - On récupère le domaine (Subject), on en fait une
-					// classe
-					// 2 - On récupère le range (Object), on en fait une classe
-					// 3 - On récupère la propriété (Property), on en récupère
-					// l'IRI
-
-					OWLObjectPropertyAssertionAxiom curAxiom2 = (OWLObjectPropertyAssertionAxiom) curAxiom;
-
-					classePrincipale = fact.getOWLClass(curAxiom2.getSubject()
-							.asOWLNamedIndividual().getIRI());
-
-					classeSecondaire = fact.getOWLClass(curAxiom2.getObject()
-							.asOWLNamedIndividual().getIRI());
-
-					iriProp = curAxiom2.getProperty().getNamedProperty()
-							.getIRI();
-					if (debug) {
-						System.out.println("SUJET : " + curAxiom2.getSubject());
-						System.out.println("OBJET : " + curAxiom2.getObject());
-						System.out.println("PROPERTY : "
-								+ curAxiom2.getProperty());
-					}
-					// 1 - if broader
-					if (iriProp != null
-							&& iriProp.toURI().getFragment().equals("broader")) {
-						/**
-						 * 1 - On regarde si la classe est SubClassOf Thing Si
-						 * c'est le cas, on supprime cet axiome
-						 * 
-						 * 2 - On la rattache à Thing si elle n'est pas déjà
-						 * présente dans l'ontologie 3 - On crée la relation
-						 * SubClassOf entre la classe que l'on traite et cette
-						 * deuxième classe.
-						 */
-
-						// 1 - On regarde si la classe est SubClassOf Thing
-
-						// On la recherche dans le factory
-						OWLSubClassOfAxiom rechercheThing = fact
-								.getOWLSubClassOfAxiom(classePrincipale,
-										fact.getOWLThing());
-						if (rechercheThing.getSuperClass() != null) {
-
-							if (debug) {
-								System.out
-										.println("Suppression relation Thing : CP: "
-												+ classePrincipale
-												+ " CS : "
-												+ classeSecondaire);
-							}
-
-							RemoveAxiom removeAxiom = new RemoveAxiom(
-									targetOntology, rechercheThing);
-							manager.applyChange(removeAxiom);
-						}
-
-						// 2 - On ajoute classeCree2 à Thing si elle n'existe
-						// pas
-						// dans l'ontologie
-						addToThing(classeSecondaire);
-
-						// 3 - On crée l'axiome SubClassOf entre owlClass et
-						// classe2
-						addSubClassOf(classePrincipale, classeSecondaire);
-					}
-				}
-			}
+			addRelation(indivCursor);
 			// On traite dans un second temps les annotations (prefLabel,
 			// altLabel, etc.)
-			Set<OWLAnnotation> listAnnotIndiv = indivCursor
-					.getAnnotations(originalOntology);
-			for (OWLAnnotation curAnnotIndiv : listAnnotIndiv) {
-				OWLLiteral litteral = (OWLLiteral) curAnnotIndiv.getValue();
-
-				// On transcode en fonction du type d'annotation
-
-				String typeAnnot = curAnnotIndiv.getProperty().getIRI().toURI()
-						.getFragment();
-
-				String value = litteral.getLiteral();
-
-				// On crée la classe à laquelle on veut rattacher l'annotation
-				OWLClass classeAnot = fact.getOWLClass(iriIndiv);
-				
-				switch (typeAnnot) {
-				case "prefLabel":
-					// System.out.println("Transcodage de prefLabel -> label");
-					addLabel(classeAnot, value);
-					break;
-				case "altLabel":
-					// System.out.println("Transcodage de altLabel -> label");
-					addLabel(classeAnot, value);
-					break;
-				case "definition":
-					// System.out.println("Transcodage de definition -> comment");
-					addComment(classePrincipale, value);
-					break;
-				case "scopeNote":
-					// System.out.println("Transcodage de scopeNote -> comment");
-					addComment(classePrincipale, value);
-					break;
-				default:
-					System.out.println("Le type " + typeAnnot
-							+ " n'est pas supporté.");
-					break;
-				}
-
-				// on récupére le type
-				// Selon le type on crée une annotation spécifique
-
-				// On rattache cette annotation spécifique à la classe
-				// concernée.
-
-			}
+			addAnnotation(indivCursor);
 
 		}
 	}
