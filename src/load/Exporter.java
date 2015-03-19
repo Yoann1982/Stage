493c71d37139c4bbd1543d645957e908fb6b0434
Yoann Keravec <yoann.keravec@gmail.com>
2015-03-19 13:27:13 +0100

    Création de la classe Exporter

---
+++ src/load/Exporter.java
@@ -0,0 +1,182 @@
+package load;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exporter {
+
+	protected List<FormatTable> listeFormat = new ArrayList<FormatTable>();
+	
+	public String stringNull(Object valeur) {
+
+		if (valeur == null)
+			return "";
+		else
+			return valeur.toString();
+	}
+	
+	public boolean checkFormat(FormatTable format, Object valeur) {
+
+		// On parcours la liste de format pour retrouver les colonnes à traiter
+
+		String colonne = format.getColumn();
+		String type = format.getType();
+		int taille = format.getTaille();
+		boolean isNullable = format.isNullable();
+
+		boolean formatOK = true;
+		if (valeur != null) {
+
+
+			String classe = valeur.getClass().toString();
+			classe = classe.substring(classe.lastIndexOf(".") + 1);
+			if (!classe.equalsIgnoreCase(type)) {
+				System.err
+						.println("Erreur : Le format de la metadata " + colonne
+								+ " de valeur " + valeur
+								+ " ne correspond pas au format attendu :"
+								+ type + ".");
+				System.err.println("Les Metadatas ne seront pas exportés.");
+				formatOK = false;
+			} else {
+				// Format OK
+				// Contrôle de la taille
+				if (taille != -1) {
+					// On vérifie la taille
+					if (valeur.toString().length() > taille) {
+						System.err.println("Erreur : La taille de la metadata "
+								+ colonne + " de valeur " + valeur
+								+ " ne correspond pas à la taille attendue :"
+								+ taille + ".");
+						System.err
+								.println("Les Metadatas ne seront pas exportés.");
+						formatOK = false;
+					}
+				}
+			}
+		} else {
+			if (!isNullable) {
+				System.err.println("Erreur : La colonne " + colonne
+						+ " ne peut pas être nulle.");
+				System.err.println("Les Metadatas ne seront pas exportés.");
+			}
+		}
+		return formatOK;
+	}
+	
+	public int getTaille(String type) {
+
+		int taille = -1;
+		if (type != null) {
+			if (type.toLowerCase().contains("VARCHAR2".toLowerCase())
+					|| type.toLowerCase().contains("CHAR".toLowerCase())) {
+
+				taille = Integer.parseInt(type.substring(type.indexOf("(") + 1,
+						type.indexOf(")")));
+
+				/*
+				 * System.out.println("Taille : " + type.matches("(.*)"));
+				 * System.out.println("REGEX : " + "\"([^\"]*)\"");
+				 * System.out.println("REGEX2 : " + "[^\\(\\)]+");
+				 * System.out.println("REGEX3 : " + "<.*>(.*)<.*>"); //
+				 * "/(\\(.*\\))/g"
+				 * 
+				 * Matcher m = Pattern.compile(""\\((.*?)\\)""").matcher(type);
+				 * while (m.find()) { System.out.println("Taille 2  : " +
+				 * m.group(0)); }
+				 */
+
+			} else if (type.toLowerCase().contains("NUMBER".toLowerCase())) {
+				taille = Integer.parseInt(type.substring(type.indexOf("(") + 1,
+						type.indexOf(",")));
+			}
+		} else
+			System.err.println("Erreur : le format de la colonne est inconnu.");
+		return taille;
+	}
+	
+	public String getType(String type) {
+
+		String typeSortie = null;
+		if (type != null) {
+			if (type.toLowerCase().contains("VARCHAR2".toLowerCase()))
+				typeSortie = "String";
+			else if (type.toLowerCase().contains("DATE".toLowerCase()))
+				typeSortie = "Date";
+			else if (type.toLowerCase().contains("NUMBER".toLowerCase()))
+				typeSortie = "Integer";
+			else if (type.toLowerCase().contains("CHAR".toLowerCase()))
+				typeSortie = "String";
+			else if (type.toLowerCase().contains("CLOB".toLowerCase()))
+				typeSortie = "String";
+		} else
+			System.err.println("Erreur : le format de la colonne est inconnu.");
+		return typeSortie;
+
+	}
+	
+	/**
+	 * Cette méthode permet de charger le contenu du fichier paramètre décrivant
+	 * le format de la table Metadata
+	 * 
+	 * @param formatFile
+	 *            Fichier contenant la description du format de la table
+	 *            Metadata d'I2B2.
+	 */
+	public void readFormatFile(String formatFile) {
+
+		try {
+			// Création du flux bufférisé sur un FileReader, immédiatement suivi
+			// par un
+			// try/finally, ce qui permet de ne fermer le flux QUE s'il le
+			// reader
+			// est correctement instancié (évite les NullPointerException)
+
+			BufferedReader buff = new BufferedReader(new FileReader(formatFile));
+			try {
+				String line;
+				// Lecture du fichier ligne par ligne. Cette boucle se termine
+				// quand la méthode retourne la valeur null.
+				int numLigne = 1;
+				while ((line = buff.readLine()) != null) {
+					// on ignore l'entête
+					if (numLigne != 1) {
+						// On alimente un objet FormatTable
+						String[] contenuLigne = line.split(";");
+						if (contenuLigne.length == 3) {
+							FormatTable formatTable = new FormatTable();
+							formatTable.setColumn(contenuLigne[0]);
+							formatTable.setType(getType(contenuLigne[1]));
+							formatTable.setTaille(getTaille(contenuLigne[1]));
+							if (contenuLigne[2].equalsIgnoreCase("yes"))
+								formatTable.setNullable(true);
+							else if (contenuLigne[2].equalsIgnoreCase("no"))
+								formatTable.setNullable(false);
+							else
+								System.err
+										.println("Valeur de Nullable incohérente. Valeur dans le fichier : "
+												+ line);
+							listeFormat.add(formatTable);
+						} else {
+							System.err
+									.println("ERREUR : nombre de colonne lue incorrect. Ligne lue : "
+											+ line);
+						}
+
+					}
+					numLigne++;
+				}
+			} finally {
+				// dans tous les cas, on ferme nos flux
+				buff.close();
+			}
+		} catch (IOException ioe) {
+			// erreur de fermeture des flux
+			System.out.println("Erreur --" + ioe.toString());
+		}
+	}
+	
+}
--- src/load/MetadataToCSV.java
+++ src/load/MetadataToCSV.java
@@ -17,11 +17,22 @@ import java.util.regex.Pattern;
  * @author y.keravec
  *
  */
-public class MetadataToCSV {
+public class MetadataToCSV extends Exporter {
 
 	private FileWriter fichier;
-	private List<FormatTable> listeFormat = new ArrayList<FormatTable>();
 
+	/**
+	 * Constructeur de la classe MetadataToCSV. Le constructeur prend en entrée
+	 * le nom du fichier CSV à produire, une liste de Metadata, le caractère
+	 * séparateur à utiliser et le nom du fichier décrivant le format de la
+	 * table Metadata. La construction du fichier CSV s'effetue sur la base du
+	 * contenu du fichier de format.
+	 * 
+	 * @param fichierCSV
+	 * @param listeMetadata
+	 * @param separator
+	 * @param fichierFormat
+	 */
 	public MetadataToCSV(String fichierCSV, List<Metadata> listeMetadata,
 			String separator, String fichierFormat) {
 
@@ -53,6 +64,19 @@ public class MetadataToCSV {
 		}
 	}
 
+	/**
+	 * Cette méthode permet d'écrire un enregistrement au sein du fichier CSV en
+	 * sortie. Elle prend en entrée un enregistrement Metadata et le caractère
+	 * séparateur du fichier CSV. Les enregistrements sont traités à partir de
+	 * la liste de format de la table des métadatas. Le type et la taille de
+	 * chaque enregistrement est testés. Un test de nullité est effectué afin de
+	 * vérifier que les champs obligatoires sont bien renseignés.
+	 * 
+	 * @param meta
+	 * @see Metadata
+	 * @param separator
+	 *            Caractère séparateur.
+	 */
 	public void writeRecord(Metadata meta, String separator) {
 		// On récupère chaque champ
 		// On le compare au format attendu
@@ -69,9 +93,9 @@ public class MetadataToCSV {
 
 			if (checkFormat(format, valeur))
 				if (cpt == 0)
-					ligne += valeur;
+					ligne += stringNull(valeur);
 				else
-					ligne += separator + valeur;
+					ligne += separator + stringNull(valeur);
 			else
 				break; // On sort car on ne pourra pas écrire l'enregistement.
 			cpt++;
@@ -85,70 +109,13 @@ public class MetadataToCSV {
 		}
 	}
 
-	/*
-	 * String cHlevel = stringNull(meta.getcHLevel()); String cFullname =
-	 * stringNull(meta.getcFullName()); String cName =
-	 * stringNull(meta.getcName()); String synonym =
-	 * stringNull(meta.getcSynonym()); String visualAttributes =
-	 * stringNull(meta.getcVisualAttributes()); String totalNum =
-	 * stringNull(meta.getcTotalNum()); String basecode =
-	 * stringNull(meta.getcBaseCode()); String metadataXML =
-	 * stringNull(meta.getcMetaDataXML()); String cFacttablecolumn =
-	 * stringNull(meta.getcFactTableColumn()); String cTableName =
-	 * stringNull(meta.getcTableName()); String cColumnName =
-	 * stringNull(meta.getcColumnName()); String cColumnDataType =
-	 * stringNull(meta.getcColumnDataType()); String cOperator =
-	 * stringNull(meta.getcOperator()); String cDimcode =
-	 * stringNull(meta.getcDimCode()); String cComment =
-	 * stringNull(meta.getcComment()); String cTooltip =
-	 * stringNull(meta.getcTooltip()); String mAppliedPath =
-	 * stringNull(meta.getmAppliedPath()); String updateDate =
-	 * stringNull(meta.getUpdateDate()); String downloadDate =
-	 * stringNull(meta.getDownloadDate()); String importDate =
-	 * stringNull(meta.getImportDate()); String sourceSystem =
-	 * stringNull(meta.getSourceSystem()); String valueType =
-	 * stringNull(meta.getValueType()); String mExclusion =
-	 * stringNull(meta.getmExclusion()); String cPath =
-	 * stringNull(meta.getcPath()); String cSymbol =
-	 * stringNull(meta.getcSymbol()); String ligne = cHlevel + separator +
-	 * cFullname + separator + cName + separator + synonym + separator +
-	 * visualAttributes + separator + totalNum + separator + basecode +
-	 * separator + metadataXML + separator + cFacttablecolumn + separator +
-	 * cTableName + separator + cColumnName + separator + cColumnDataType +
-	 * separator + cOperator + separator + cDimcode + separator + cComment +
-	 * separator + cTooltip + separator + mAppliedPath + separator + updateDate
-	 * + separator + downloadDate + separator + importDate + separator +
-	 * sourceSystem + separator + valueType + separator + mExclusion + separator
-	 * + cPath + separator + cSymbol + "\n";
+	/**
+	 * Cette méthode permet d'écrire l'entête du fichier CSV. Il est constuit à
+	 * partir de la liste de Format qui liste les colonnes de la table Metadata.
 	 * 
-	 * try { fichier.write(ligne, 0, ligne.length()); } catch (IOException e) {
-	 * // TODO Auto-generated catch block e.printStackTrace(); }
+	 * @param separator
+	 *            Le caractère séparateur du fichier CSV.
 	 */
-
-	public String stringNull(String input) {
-
-		if (input == null)
-			return "";
-		else
-			return input;
-	}
-
-	public String stringNull(Date input) {
-
-		if (input == null)
-			return "";
-		else
-			return input.toString();
-	}
-
-	public String stringNull(Integer input) {
-
-		if (input == null)
-			return "";
-		else
-			return input.toString();
-	}
-
 	public void writeHeader(String separator) {
 
 		String header = "";
@@ -170,167 +137,5 @@ public class MetadataToCSV {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-
-	/**
-	 * Cette méthode permet de charger le contenu du fichier paramètre décrivant
-	 * le format de la table Metadata
-	 * 
-	 * @param formatFile
-	 *            Fichier contenant la description du format de la table
-	 *            Metadata d'I2B2.
-	 */
-	public void readFormatFile(String formatFile) {
-
-		try {
-			// Création du flux bufférisé sur un FileReader, immédiatement suivi
-			// par un
-			// try/finally, ce qui permet de ne fermer le flux QUE s'il le
-			// reader
-			// est correctement instancié (évite les NullPointerException)
-
-			BufferedReader buff = new BufferedReader(new FileReader(formatFile));
-			try {
-				String line;
-				// Lecture du fichier ligne par ligne. Cette boucle se termine
-				// quand la méthode retourne la valeur null.
-				int numLigne = 1;
-				while ((line = buff.readLine()) != null) {
-					// on ignore l'entête
-					if (numLigne != 1) {
-						// On alimente un objet FormatTable
-						String[] contenuLigne = line.split(";");
-						if (contenuLigne.length == 3) {
-							FormatTable formatTable = new FormatTable();
-							formatTable.setColumn(contenuLigne[0]);
-							formatTable.setType(getType(contenuLigne[1]));
-							formatTable.setTaille(getTaille(contenuLigne[1]));
-							if (contenuLigne[2].equalsIgnoreCase("yes"))
-								formatTable.setNullable(true);
-							else if (contenuLigne[2].equalsIgnoreCase("no"))
-								formatTable.setNullable(false);
-							else
-								System.err
-										.println("Valeur de Nullable incohérente. Valeur dans le fichier : "
-												+ line);
-							listeFormat.add(formatTable);
-						} else {
-							System.err
-									.println("ERREUR : nombre de colonne lue incorrect. Ligne lue : "
-											+ line);
-						}
-
-					}
-					numLigne++;
-				}
-			} finally {
-				// dans tous les cas, on ferme nos flux
-				buff.close();
-			}
-		} catch (IOException ioe) {
-			// erreur de fermeture des flux
-			System.out.println("Erreur --" + ioe.toString());
-		}
-	}
-
-	public String getType(String type) {
-
-		String typeSortie = null;
-		if (type != null) {
-			if (type.toLowerCase().contains("VARCHAR2".toLowerCase()))
-				typeSortie = "String";
-			else if (type.toLowerCase().contains("DATE".toLowerCase()))
-				typeSortie = "Date";
-			else if (type.toLowerCase().contains("NUMBER".toLowerCase()))
-				typeSortie = "Integer";
-			else if (type.toLowerCase().contains("CHAR".toLowerCase()))
-				typeSortie = "String";
-			else if (type.toLowerCase().contains("CLOB".toLowerCase()))
-				typeSortie = "String";
-		} else
-			System.err.println("Erreur : le format de la colonne est inconnu.");
-		return typeSortie;
-
-	}
-
-	public int getTaille(String type) {
-
-		int taille = -1;
-		if (type != null) {
-			if (type.toLowerCase().contains("VARCHAR2".toLowerCase())
-					|| type.toLowerCase().contains("CHAR".toLowerCase())) {
-
-				taille = Integer.parseInt(type.substring(type.indexOf("(") + 1,
-						type.indexOf(")")));
-
-				/*
-				 * System.out.println("Taille : " + type.matches("(.*)"));
-				 * System.out.println("REGEX : " + "\"([^\"]*)\"");
-				 * System.out.println("REGEX2 : " + "[^\\(\\)]+");
-				 * System.out.println("REGEX3 : " + "<.*>(.*)<.*>"); //
-				 * "/(\\(.*\\))/g"
-				 * 
-				 * Matcher m = Pattern.compile(""\\((.*?)\\)""").matcher(type);
-				 * while (m.find()) { System.out.println("Taille 2  : " +
-				 * m.group(0)); }
-				 */
-
-			} else if (type.toLowerCase().contains("NUMBER".toLowerCase())) {
-				taille = Integer.parseInt(type.substring(type.indexOf("(") + 1,
-						type.indexOf(",")));
-			}
-		} else
-			System.err.println("Erreur : le format de la colonne est inconnu.");
-		return taille;
-	}
-
-	public boolean checkFormat(FormatTable format, Object valeur) {
-
-		// On parcours la liste de format pour retrouver les colonnes à traiter
-
-		String colonne = format.getColumn();
-		String type = format.getType();
-		int taille = format.getTaille();
-		boolean isNullable = format.isNullable();
-
-		boolean formatOK = true;
-		if (valeur != null) {
-
-
-			String classe = valeur.getClass().toString();
-			classe = classe.substring(classe.lastIndexOf(".") + 1);
-			if (!classe.equalsIgnoreCase(type)) {
-				System.err
-						.println("Erreur : Le format de la metadata " + colonne
-								+ " de valeur " + valeur
-								+ " ne correspond pas au format attendu :"
-								+ type + ".");
-				System.err.println("Les Metadatas ne seront pas exportés.");
-				formatOK = false;
-			} else {
-				// Format OK
-				// Contrôle de la taille
-				if (!type.equalsIgnoreCase("Date")
-						|| type.equalsIgnoreCase("CLOB")) {
-					// On vérifie la taille
-					if (valeur.toString().length() > taille) {
-						System.err.println("Erreur : La taille de la metadata "
-								+ colonne + " de valeur " + valeur
-								+ " ne correspond pas à la taille attendue :"
-								+ taille + ".");
-						System.err
-								.println("Les Metadatas ne seront pas exportés.");
-						formatOK = false;
-					}
-				}
-			}
-		} else {
-			if (!isNullable) {
-				System.err.println("Erreur : La colonne " + colonne
-						+ " ne peut pas être nulle.");
-				System.err.println("Les Metadatas ne seront pas exportés.");
-			}
-		}
-		return formatOK;
 	}
 }
